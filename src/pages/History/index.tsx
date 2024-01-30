f02fd748940d34d41cdb49b41682fc43ff1b3c06
Fernando Lima <eufernandoprogramador@gmail.com>
2024-01-30 01:21:39 -0300

    feat: context between routes

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,17 @@
+import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './styles'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
+
+      <pre>
+        <code>{JSON.stringify(cycles, null, 2)}</code>
+      </pre>
 
       <HistoryList>
         <table>
