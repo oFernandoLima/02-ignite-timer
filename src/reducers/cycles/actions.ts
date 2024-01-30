5549e5bbd66b613a75a4498385fd615eaa4a95f2
Fernando Lima <eufernandoprogramador@gmail.com>
2024-01-30 15:25:26 -0300

    fix: action typing in reducer

--- src/reducers/cycles/actions.ts
+++ src/reducers/cycles/actions.ts
@@ -5,8 +5,25 @@ export enum ActionTypes {
   INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
   MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
 }
+interface newCycleActionProps {
+  type: ActionTypes.ADD_NEW_CYCLE
+  payload: { newCycle: Cycle }
+}
 
-export function addNewCycleAction(newCycle: Cycle) {
+interface markCurrentCycleFinishedProps {
+  type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED
+}
+
+interface interruptedCycleActionProps {
+  type: ActionTypes.INTERRUPT_CURRENT_CYCLE
+}
+
+export type ActionsProps =
+  | newCycleActionProps
+  | markCurrentCycleFinishedProps
+  | interruptedCycleActionProps
+
+export function addNewCycleAction(newCycle: Cycle): newCycleActionProps {
   return {
     type: ActionTypes.ADD_NEW_CYCLE,
     payload: {
@@ -15,13 +32,13 @@ export function addNewCycleAction(newCycle: Cycle) {
   }
 }
 
-export function interruptCurrentCycleAction() {
+export function interruptCurrentCycleAction(): interruptedCycleActionProps {
   return {
     type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
   }
 }
 
-export function markCurrentCycleAsFinishedAction() {
+export function markCurrentCycleAsFinishedAction(): markCurrentCycleFinishedProps {
   return {
     type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
   }
